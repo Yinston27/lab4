9b5684e94adb4d9df8ca5ac885e9af43ea9b564f
Yinston <n3152u@gmail.com>
2025-11-26 18:56:52 +0300

    доработана функция рюкзака, повышена универсальность

--- lab_4.py
+++ lab_4.py
@@ -13,9 +13,11 @@ item = {
     'c':{'price':20, 'weight':2},
     
 }
-
+LENGTH = 2
+WIDTH = 4
 
 def gen_memo_table(items, max_weight, required_item=None, basic_point=20):
+    list_change = None
     if required_item in items:
         list_change = items.pop(required_item)
         max_weight -= list_change['weight']
@@ -45,11 +47,11 @@ def gen_memo_table(items, max_weight, required_item=None, basic_point=20):
                     table[i][limit_idx] = max(res, prev_price)
     # for row in table:
     #     print(row)
-    return [items, table, return_iterator, max_weight, required_item, list_change, basic_point]
+    return [items, table, return_iterator, max_weight, required_item, basic_point, list_change]
 
 
-def return_list(items, table, return_iterator, max_weight, required_item, list_change, basic_point):
-    if len(list_change) > 0:
+def return_list(items, table, return_iterator, max_weight, required_item, basic_point, list_change=None):
+    if required_item:
         prev_bag = [required_item for i in range(list_change['weight'])]
     else:
         prev_bag = []
@@ -60,34 +62,34 @@ def return_list(items, table, return_iterator, max_weight, required_item, list_c
     for i, j in return_iterator:
         d[i] = j
 
-    while max_weight > 0 and item_idx >= 0:
+    while item_idx >= 0:
         weight = items[d[item_idx]]['weight']
         price = items[d[item_idx]]['price']
-        if item_idx != 0 and table[item_idx-1][limit_weight] != table[item_idx][limit_weight] or (item_idx == 0 and table[item_idx][limit_weight] > 0):
-            for i in range(weight):
-                prev_bag.append(d[item_idx])
-            max_weight -= weight
-            basic_point += price
-            limit_weight -= weight
+        if max_weight > 0:
+            if item_idx != 0 and table[item_idx-1][limit_weight] != table[item_idx][limit_weight] or (item_idx == 0 and table[item_idx][limit_weight] > 0):
+                for i in range(weight):
+                    prev_bag.append(d[item_idx])
+                max_weight -= weight
+                basic_point += price
+                limit_weight -= weight
+            else:
+                negative_point += price
+            
         else:
             negative_point += price
         item_idx -= 1
-    return_bag = [[[] for i in range(3)] for j in range(3)]
+
+    return_bag = [[[] for i in range(WIDTH)] for j in range(LENGTH)]
     for i in range(len(prev_bag)):
-        return_bag[i // 3][i % 3].append(prev_bag[i])
+        return_bag[i // WIDTH][i % WIDTH].append(prev_bag[i])
     for row in return_bag:
         print(row)
     
     print(f'Очки выживания: {basic_point - negative_point}')
-
-            
-
-
-
-
+  
 
 if __name__ == '__main__':
-    res = gen_memo_table(item, 9, 'd')
+    res = gen_memo_table(item, WIDTH*LENGTH)
     # for i in res[1]:
     #     print(i)
     return_list(res[0], res[1], res[2], res[3], res[4], res[5], res[6])
